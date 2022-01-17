5fb99d59c264064b8042d050291591265055e395
KushalRaut <kushal.074bct025@acem.edu.np>
2022-01-17 12:02:27 +0545

    unauthorized bug fix

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -9,6 +9,6 @@ const {
 const { isAuthenticatedUser } = require('../middlewares/auth')
 
 router.route('/payment/process').post(isAuthenticatedUser, processPayment)
-router.route('/stripeapi').get(isAuthenticatedUser, sendStripeApi)
+router.route('/stripeapi').get(sendStripeApi)
 
 module.exports = router
